5014b377efa04abc8fcf5530e1fef06b6a817ba1
astromberg1 <astromberg1@hotmail.com>
2016-12-01 13:34:41 +0100

    1 incheck

---
+++ kunskapscheckMVC/kunskapscheckMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kunskapscheckMVC.Startup))]
+namespace kunskapscheckMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
